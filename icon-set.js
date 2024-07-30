5e5aeb4f34c5d61c892e070b07331405e69c543f
erickmerchant <erickmerchant@gmail.com>
2024-07-30 11:57:44 -0400

    Use files

---
+++ icon-set.js
@@ -0,0 +1,95 @@
+class IconSet extends HTMLElement {
+	static #sheet = new CSSStyleSheet();
+
+	static {
+		customElements.define("icon-set", IconSet);
+
+		this.#sheet.replaceSync(`
+			* {
+				box-sizing: border-box;
+				margin: 0;
+				padding: 0;
+				font: inherit;
+				max-inline-size: 100%;
+			}
+
+			button {
+					appearance: none;
+					background: hsl(0 0 0 / 0);
+					border: none;
+			}
+		`);
+
+		let cssUrl = new URL("./icon-set.css", import.meta.url);
+
+		fetch(cssUrl)
+			.then((res) => res.text())
+			.then((css) => {
+				this.#sheet.replaceSync(css);
+			});
+	}
+
+	#timeout;
+	#popover;
+
+	connectedCallback() {
+		let shadow = this.attachShadow({mode: "open", slotAssignment: "manual"});
+		let i = 0;
+
+		for (let svg of this.querySelectorAll("svg")) {
+			let slot = document.createElement("slot");
+
+			slot.setAttribute("name", `icon-${i++}`);
+
+			let button = document.createElement("button");
+
+			button.append(slot);
+			shadow.append(button);
+
+			slot.assign(svg);
+
+			button.addEventListener("click", (e) => {
+				this.dispatchEvent(new CustomEvent("copy", {detail: {button}}));
+			});
+
+			this.addEventListener("copy", async (e) => {
+				let clicked = e.detail?.button === button;
+
+				button.classList.toggle("clicked", clicked);
+
+				if (!clicked) {
+					return;
+				}
+
+				this.#popover?.showPopover();
+
+				if (this.#timeout) {
+					clearTimeout(this.#timeout);
+				}
+
+				this.#timeout = setTimeout(() => {
+					this.dispatchEvent(new CustomEvent("copy", {detail: {button: null}}));
+
+					this.#popover?.hidePopover();
+				}, 2_000);
+
+				try {
+					await navigator.clipboard.writeText(button.innerHTML?.trim?.());
+				} catch (_) {}
+			});
+		}
+
+		let div = document.createElement("div");
+		let span = document.createElement("span");
+		let checkIcon = this.querySelector('[data-icon="check"]').cloneNode(true);
+
+		div.toggleAttribute("popover", true);
+		span.append("Copied");
+		div.append(checkIcon, span);
+
+		this.#popover = div;
+
+		shadow.adoptedStyleSheets = [IconSet.#sheet];
+		shadow.append(div);
+	}
+}
