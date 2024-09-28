ac53a748d6cd5d710762435ea57b8e9d765e9238
erickmerchant <erickmerchant@gmail.com>
2024-09-28 00:07:04 -0400

    Changes

--- em-icon-set.js
+++ em-icon-set.js
@@ -1,54 +1,60 @@
 class EmIconSet extends HTMLElement {
+	#timeout;
+
 	connectedCallback() {
 		let shadow = this.shadowRoot;
 		let i = 0;
-		let timeout;
+
+		shadow.addEventListener("click", this);
 
 		for (let button of shadow.querySelectorAll("button")) {
 			i += 1;
 
-			let svg = button.querySelector("svg");
+			let points = Array.from({length: 2}, () =>
+				(Math.random() * 0.8 - 0.4).toPrecision(5)
+			).join(" ");
 
-			if (svg) {
-				let popover = document.createElement("div");
+			button.style.setProperty("--light", `oklab(95% ${points})`);
+			button.style.setProperty("--dark", `oklab(5% ${points})`);
+			button.style.setProperty("--anchor-name", `--button-${i}`);
+		}
+	}
+
+	handleEvent(e) {
+		let button = e.target.closest("button");
+
+		if (button) {
+			let svg = button.querySelector("svg");
+			let popover = button.querySelector("[popover]");
+			let clipboard = svg ? svg.outerHTML.trim() : "";
+
+			button.classList.add("clicked");
+
+			if (this.#timeout) {
+				clearTimeout(this.#timeout);
+			}
+
+			if (!popover) {
+				popover = document.createElement("div");
 
 				popover.toggleAttribute("popover", true);
 				popover.append("Copied");
 
 				button.append(popover);
-
-				let points = Array.from({length: 2}, () =>
-					(Math.random() * 0.8 - 0.4).toPrecision(5)
-				).join(" ");
-
-				button.style.setProperty("--light", `oklab(95% ${points})`);
-				button.style.setProperty("--dark", `oklab(5% ${points})`);
-				button.style.setProperty("--anchor-name", `--button-${i}`);
-
-				let clipboard = svg.outerHTML.trim();
-
-				button.addEventListener("click", async (e) => {
-					button.classList.add("clicked");
-
-					if (timeout) {
-						clearTimeout(timeout);
-					}
-
-					popover.showPopover();
-
-					timeout = setTimeout(() => {
-						popover.hidePopover();
-
-						button.classList.remove("clicked");
-					}, 2_000);
-
-					try {
-						await navigator.clipboard.writeText(clipboard);
-					} catch (_) {}
-				});
 			}
+
+			popover.showPopover();
+
+			this.#timeout = setTimeout(() => {
+				popover.hidePopover();
+
+				button.classList.remove("clicked");
+			}, 2_000);
+
+			navigator.clipboard.writeText(clipboard).finally((_) => {});
 		}
 	}
 }
 
 customElements.define("em-icon-set", EmIconSet);
+customElements.define("em-icons", class extends HTMLElement {});
