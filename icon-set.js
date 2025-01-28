8def0bc24fa871ef5acb9a85dd7d1481028be374
erickmerchant <erickmerchant@gmail.com>
2025-01-27 22:32:23 -0500

    Use unreleased v5 vanilla-kit

--- icon-set.js
+++ icon-set.js
@@ -1,4 +1,8 @@
-import {define, effect, html, watch} from "vanilla-kit";
+import {effect, html, watch} from "vanilla-kit/prelude/min.js";
+import {define} from "vanilla-kit/define.js";
+import "vanilla-kit/element/classes.js";
+import "vanilla-kit/element/observe.js";
+import "vanilla-kit/element/styles.js";
 
 let {div} = html;
 
@@ -6,44 +10,48 @@ define("icon-set").connected((host) => {
 	let timeout;
 	let state = watch({clicked: -1});
 	let i = 0;
+	let observed = host.observe();
+	let buttons = observed.find(":scope > button");
 
-	for (let button of host.find("button")) {
-		let index = i++;
+	effect(() => {
+		for (let button of buttons) {
+			let index = i++;
 
-		let points = Array.from({length: 2}, () =>
-			(Math.random() * 0.8 - 0.4).toPrecision(5)
-		).join(" ");
+			let points = Array.from({length: 2}, () =>
+				(Math.random() * 0.8 - 0.4).toPrecision(5)
+			).join(" ");
 
-		let popover = div().attr("popover", true).text("Copied");
-		let content = button.deref().innerHTML.trim();
+			let popover = div().attr("popover", true).text("Copied");
+			let content = button.deref().innerHTML.trim();
 
-		button
-			.classes({clicked: () => state.clicked === index})
-			.on("click", (e) => {
-				state.clicked = index;
-			})
-			.styles({
-				"--light": `oklab(95% ${points})`,
-				"--dark": `oklab(5% ${points})`,
-				"--anchor-name": `--button-${index}`,
-			})
-			.nodes(popover);
+			button
+				.classes({clicked: () => state.clicked === index})
+				.on("click", (e) => {
+					state.clicked = index;
+				})
+				.styles({
+					"--light": `oklab(95% ${points})`,
+					"--dark": `oklab(5% ${points})`,
+					"--anchor-name": `--button-${index}`,
+				})
+				.nodes(...button.deref().children, popover);
 
-		effect(() => {
-			if (state.clicked === index) {
-				if (timeout) {
-					clearTimeout(timeout);
+			effect(() => {
+				if (state.clicked === index) {
+					if (timeout) {
+						clearTimeout(timeout);
+					}
+
+					popover.deref().showPopover();
+					navigator.clipboard.writeText(content).finally((_) => {});
+
+					timeout = setTimeout(() => {
+						state.clicked = -1;
+					}, 2_000);
+				} else {
+					popover.deref().hidePopover();
 				}
-
-				popover.deref().showPopover();
-				navigator.clipboard.writeText(content).finally((_) => {});
-
-				timeout = setTimeout(() => {
-					state.clicked = -1;
-				}, 2_000);
-			} else {
-				popover.deref().hidePopover();
-			}
-		});
-	}
+			});
+		}
+	});
 });
