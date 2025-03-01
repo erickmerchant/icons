4ac89a10198c79bfaf0403a5a716b532354eff29
erickmerchant <erickmerchant@gmail.com>
2025-02-28 22:47:47 -0500

    Rewrite toast

--- icon-set.js
+++ icon-set.js
@@ -1,6 +1,4 @@
 import {html} from "handcraft/dom.js";
-import {watch, effect} from "handcraft/reactivity.js";
-import {define} from "handcraft/define.js";
 import "handcraft/dom/attr.js";
 import "handcraft/dom/classes.js";
 import "handcraft/dom/effect.js";
@@ -9,56 +7,58 @@ import "handcraft/dom/observe.js";
 import "handcraft/dom/on.js";
 import "handcraft/dom/styles.js";
 import "handcraft/dom/text.js";
+import {watch, effect} from "handcraft/reactivity.js";
+import {define} from "handcraft/define.js";
 
 let {div: DIV} = html;
 
 define("icon-set").connected((host) => {
 	let timeout;
-	let state = watch({clicked: -1});
-	let i = 0;
+	let state = watch({color: null, anchorName: null});
+	let i = -1;
 	let observed = host.observe();
 	let buttons = observed.find(":scope > button");
+	let popover = DIV()
+		.attr("popover", true)
+		.styles({
+			"--color": () => state.color,
+			"--anchor-name": () => state.anchorName,
+		})
+		.text("Copied")
+		.effect((el) => {
+			if (state.color != null) {
+				if (timeout) {
+					clearTimeout(timeout);
+				}
+
+				el.showPopover();
+
+				timeout = setTimeout(() => {
+					state.color = null;
+				}, 2_000);
+			} else {
+				el.hidePopover();
+			}
+		});
+
+	host.nodes(popover);
 
 	effect(() => {
 		for (let button of buttons) {
-			let index = i++;
+			let anchorName = `--button-${++i}`;
 
-			let points = Array.from({length: 2}, () =>
+			let color = Array.from({length: 2}, () =>
 				(Math.random() * 0.8 - 0.4).toPrecision(5)
 			).join(" ");
 
-			let popover = DIV()
-				.attr("popover", true)
-				.text("Copied")
-				.effect((el) => {
-					if (state.clicked === index) {
-						if (timeout) {
-							clearTimeout(timeout);
-						}
-
-						el.showPopover();
-
-						timeout = setTimeout(() => {
-							state.clicked = -1;
-						}, 2_000);
-					} else {
-						el.hidePopover();
-					}
-				});
-
 			button
-				.classes({clicked: () => state.clicked === index})
-				.styles({
-					"--light": `oklab(95% ${points})`,
-					"--dark": `oklab(5% ${points})`,
-					"--anchor-name": `--button-${index}`,
-				})
-				.nodes(popover)
-				.on("click", (e) => {
-					state.clicked = index;
+				.styles({"--color": color, "anchor-name": anchorName})
+				.on("click", () => {
+					state.color = color;
+					state.anchorName = anchorName;
 				})
 				.effect((el) => {
-					if (state.clicked === index) {
+					if (state.color === color) {
 						navigator.clipboard
 							.writeText(el.innerHTML.trim())
 							.finally((_) => {});
